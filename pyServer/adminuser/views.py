0522e114067985fe7ef2d7fe6ad4b99c479e24c4
kimduhee <oo2706@naver.com>
2025-06-12 09:35:11 +0900

    수정정

--- pyServer/adminuser/views.py
+++ pyServer/adminuser/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from django.db.models import Q
 from django.http import JsonResponse
 from django.core import serializers
 from django.shortcuts import HttpResponse, render
@@ -21,12 +22,30 @@ def adminuserList(request):
 
         #페이지 번호
         pageNo = requestData.get('pageNo')
+        searchText = requestData.get('searchText')
         if not pageNo:
             pageNo = 1
 
-        print("pageNo: %s" % str(pageNo))
+        print("request parameter pageNo: %s" % str(pageNo))
+        print("request parameter searchText: %s" % searchText)
 
-        result = AdminUser.objects.all()
+        """
+        result = None
+        if not searchText:
+            result = AdminUser.objects.all()
+        else:
+            result = AdminUser.objects.filter(admin_id = searchText)
+        """
+        #result = AdminUser.objects.raw("SELECT * FROM ADMIN_USER")
+        
+        obj = Q()
+        if searchText:
+            obj.add(Q(admin_id = searchText), obj.AND)
+
+        result = AdminUser.objects.filter(obj)
+
+        print("query: %s" % result.query)
+
         pages = Paginator(result, 10)
         page = pages.get_page(pageNo)
 
@@ -35,11 +54,11 @@ def adminuserList(request):
 
         result = {
             "adminList": resultJson,
-            "has_next": page.has_next(),
-            "has_previous": page.has_previous(),
-            "next_page_number": page.next_page_number() if page.has_next() else None,
-            "previous_page_nymber": page.previous_page_number() if page.has_previous() else None,
-            "num_pages": pages.num_pages
+            #"has_next": page.has_next(),
+            #"has_previous": page.has_previous(),
+            #"next_page_number": page.next_page_number() if page.has_next() else None,
+            #"previous_page_nymber": page.previous_page_number() if page.has_previous() else None,
+            "totalPage": pages.num_pages
         }
 
         return commHttpResponse(200, result)
--- pyServer/static/dist/js/common_biz.js
+++ pyServer/static/dist/js/common_biz.js
@@ -15,3 +15,52 @@ const getAjaxData = (ajaxUrl, ajaxData, callback) => {
         }
     });
 }
+
+const pagingCreate = (pageNo, totalPageNo) => {
+
+      let startPage = (parseInt(pageNo/10)*10) + 1;
+      let endPage = (startPage + 9) > totalPageNo?totalPageNo:startPage + 9;
+
+      console.log("startPage:" + startPage);
+      console.log("endPage:" + endPage);
+
+      let pagehtml = "";
+      pagehtml += '<div class="col-sm-12 col-md-7">';
+      pagehtml += ' <div class="dataTables_paginate paging_simple_numbers">';
+      pagehtml += '   <ul class="pagination">';
+
+      //첫 페이지
+      pagehtml += '     <li class="paginate_button page-item previous"><a href="#" class="page-link"><<</a></li>';
+      
+      //이전 페이지
+      if(pageNo == 1 || totalPageNo == 1) {
+        pagehtml += '     <li class="paginate_button page-item previous disabled"><a href="#" class="page-link"><</a></li>';
+      } else {
+        pagehtml += '     <li class="paginate_button page-item previous"><a href="#" class="page-link"><</a></li>';
+      }
+
+      //페이지 처리
+      for(var i=startPage;i<endPage+1; i++) {
+        if(i==pageNo) {
+          pagehtml += '     <li class="paginate_button page-item active"><a href="javascript:void(0);" class="page-link">'+i+'</a></li>';
+        } else {
+          pagehtml += '     <li class="paginate_button page-item"><a href="#" class="page-link">'+i+'</a></li>';
+        }
+      }
+
+      //다음 페이지
+      if(totalPageNo > pageNo) {
+        pagehtml += '     <li class="paginate_button page-item next"><a href="#" class="page-link">></a></li>';
+      } else {
+        pagehtml += '     <li class="paginate_button page-item next disabled"><a href="#" class="page-link">></a></li>';
+      }
+
+      //마지막 페이지
+      pagehtml += '     <li class="paginate_button page-item next"><a href="#" class="page-link">>></a></li>';
+
+      pagehtml += '   </ul>';
+      pagehtml += ' </div>';
+      pagehtml += '</div>';
+
+      return pagehtml;
+    }
